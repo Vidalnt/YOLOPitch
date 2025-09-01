d95fa01b9995243216b787eb5c8b82c85706ee66
vidalnt <raddiscord1@gmail.com>
2025-09-01 12:47:43 -0500

    Update get_ark.py

--- DataProcessing/get_ark.py
+++ DataProcessing/get_ark.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import random
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from formula_all import *
 
 def main(path, label_path):
